47755f6ecf2e8d530b33534d8f2f0a5f4260556a
ssoyeeon <ssuu2039@gmail.com>
2024-07-04 18:59:17 +0900

    [feat] 오류는 뜨지만 상관없어요 ㅎㅎ~
    바닥 만들어주고 그 위에 깃발 비스무리 한 생명체 만들어서 움직이기.

---
+++ Assets/Sc/MoveOne.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOne : MonoBehaviour
+{
+    public Rigidbody rig;
+
+    public float F1 = 10;
+ 
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            rig.AddForce(Vector3.forward * F1, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rig.AddForce(Vector3.left * F1, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rig.AddForce(Vector3.right * F1, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rig.AddForce(Vector3.back * F1, ForceMode.Force);
+        }
+    }
+}
